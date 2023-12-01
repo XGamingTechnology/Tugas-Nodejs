7302b0bcf247cacfe418df88e4afa564b1336835
Kiky <rizkyramdani1402@gmai.com>
2023-12-01 16:19:26 +0700

    membuat fungsi async

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -3,17 +3,13 @@ const { User } = require('../../models')
 
 // uuidv4();
 
-const getAllUser = (req, res) => {
-    console.log('ini jadi apa', User)
-    User.findAll()
-        .then(result => {
-            res.json({result})
-        })
-        .catch(err => {
-            res.status(500).json({ message: 'internal Server Error'})
-            console.log("err", err )
-
-        })
+const getAllUser = async (req, res) => {
+    try {
+        const user = await User.findAll()   
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json ({ message: 'Internal Server Error'})
+    }
 }
 
 const createUser = (req, res) => {
