81578b57969c413336c5de5935c50fdd60ff687b
Kiky <rizkyramdani1402@gmai.com>
2023-12-07 00:41:24 +0700

    menambahkan @swagger

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,18 @@ const router = express.Router()
 
 const UserController = require('../Controllers/user.controller')
 
+/**
+ * @swagger
+ * /users:
+ * get:
+ *         description: Get all user
+ *         responses:
+ *              200:
+ *                  description: Success mendapat data
+ *              500:
+ *                  description: Internal Server error
+ */
+
 router.get('/user', UserController.getAllUser)
 router.post('/user', UserController.createUser)
 router.put('/user/:id', UserController.updateUser)
