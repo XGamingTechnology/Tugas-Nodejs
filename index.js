2ddb0285e430cbfa1a1044d569842b61997863cd
Kiky <rizkyramdani1402@gmai.com>
2023-12-06 14:26:23 +0700

    memanggil fungsi router di index.js

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ app.use(express.json())
 // import router
 
 const userRouter = require('./src/routes/user.routes')
+const nationRouter = require('./src/routes/nation.routes')
 app.use('/api', userRouter)
-
+app.use('/api', nationRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
