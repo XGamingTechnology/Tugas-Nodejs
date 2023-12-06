0baff8501edc5b3ee3b20c72a4fc9848f66eb481
Kiky <rizkyramdani1402@gmai.com>
2023-12-07 02:33:11 +0700

    Menambahkan fungsi put dalam swager

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,6 +17,38 @@ const UserController = require('../Controllers/user.controller')
 
 router.get('/user', UserController.getAllUser)
 router.post('/user', UserController.createUser)
+
+/**
+ * @swagger
+ * /api/user/{id}:
+ *   put:
+ *     description: Update user
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: ID user
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fullName:
+ *                 type: string
+ *                 description: Nama lengkap
+ *               email:
+ *                 type: string
+ *                 description: Email
+ *     responses:
+ *       200:
+ *         description: Success ubah data
+ *       500:
+ *         description: Internal Server error
+ */
+
 router.put('/user/:id', UserController.updateUser)
 
 module.exports =router
